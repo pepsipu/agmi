d343e9c826809e5923efa3d7a44f0c2d9c640427
Sammy Hajhamid <sh@pepsi.pw>
2026-03-31 18:30:34 -0700

    Initial Fabric mod starter

---
+++ src/client/java/agmi/AgmiClient.java
@@ -0,0 +1,10 @@
+package agmi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AgmiClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
